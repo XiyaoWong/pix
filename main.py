695670f431c6ee6256cb70d6dc51e894154f754f
wongxy <xiyao.wong@foxmail.com>
2020-05-10 11:02:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ try:
     parse.main()
     dl.main()
 except Exception:
-    pass
+    pics = os.listdir(config.IMAGES_SAVE_DIR)
+    data = {
+        'pics': pics,
+        'last_update': int(time.time()),
+        'count': len(pics)
+    }
+    with open(os.path.join(config.BASE_DIR, 'data.json'), 'w') as f:
+        json.dump(data, f)
+    print('更新data.json成功')
 finally:
     pics = os.listdir(config.IMAGES_SAVE_DIR)
     data = {
