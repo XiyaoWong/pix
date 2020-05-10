20e44b1a9a69731994ced107d43a8750ace6a3f1
wongxy <xiyao.wong@foxmail.com>
2020-05-10 09:15:57 +0800

    update

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import dl
 import json
 import os
+import sys
 import time
 
 import config
 import parse
 
-parse.main()
-dl.main()
-
-pics = os.listdir(config.IMAGES_SAVE_DIR)
-
-data = {
-    'pics': pics,
-    'last_update': int(time.time()),
-    'count': len(pics)
-}
-
-with open(os.path.join(config.BASE_DIR, 'data.json'), 'w') as f:
-    json.dump(data, f)
+try:
+    parse.main()
+    dl.main()
+except KeyboardInterrupt:
+    print("正在退出...")
+    sys.exit()
+finally:
+    pics = os.listdir(config.IMAGES_SAVE_DIR)
+    data = {
+        'pics': pics,
+        'last_update': int(time.time()),
+        'count': len(pics)
+    }
+    with open(os.path.join(config.BASE_DIR, 'data.json'), 'w') as f:
+        json.dump(data, f)
+    print('更新data.json成功')
